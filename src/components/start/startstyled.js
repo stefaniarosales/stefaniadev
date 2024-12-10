4cca6b4a40f7f8c28be776c3c051b84fca29a3db
stefaniarosales <stefaniarosales@live.com>
2024-12-10 15:27:23 -0300

    mod: start styles

--- src/components/start/startstyled.js
+++ src/components/start/startstyled.js
@@ -16,44 +16,43 @@ export const ContainerNameStyled = styled.div`
     backdrop-filter: blur(8px) saturate(180%);
     -webkit-backdrop-filter: blur(8px) saturate(180%);
     background-color: rgba(255, 255, 255, 0.35);
-    position: fixed;
-    width: 80%;
-    height: 25%;
-    top: 450px;
-    right: 500px;
-    padding: 1rem;
+    width: 100%;
+    height: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    @media (max-width: 768px) {
-        top: 50%;
-        right: 10%;
-    }
 `
 export const TitleStyled = styled.h1`
-    font-size: 5rem;
+    font-size: 7rem;
     font-weight: bold;
     margin: 0;
     color: #B7B7B7;
     letter-spacing: 3px;
 
     @media (max-width: 768px) {
-        font-size: 3rem;
+        font-size: 5rem;
     }
 `
 
 export const SubTitleStyled = styled.h2`
     font-weight: lighter;
-    font-size: 50px;
+    font-size: 4rem;
     margin: 0;
     color: #B7B7B7;
+    @media (max-width: 768px) {
+        font-size: 3rem;
+    }
 `
 
 export const ContainerImg = styled.div`
-    width: 50%;
+    width: 70%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items:center;
     background-image: url(/img/stefania.jpg);
-    background-position: center center;
+    background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
 
