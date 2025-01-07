4434ce88def214be2c98b99e4d7dbc2b4fbda4f8
stefaniarosales <stefaniarosales@live.com>
2025-01-07 15:12:45 -0300

    add: illustration skills

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
 import { ContainerLineTitle, Line, Title } from '../about/aboutstyled'
-import { Container, ContainerSkills,ContainerSoftSkills, ContainerTechnicalSkills, TitleSoftSkills, TitleTechnicalSkills } from './skillsstyled'
+import { 
+Container,
+ContainerSkills,
+ContainerSoftSkills,
+ContainerTechnicalSkills, 
+SubContainerTechnicalSkills,
+SubContainerSoftSkills, 
+TitleSoftSkills, 
+TitleTechnicalSkills,
+ContainerIllustrationTitle,
+Illustration,
+TitleIllustration
+} from './skillsstyled'
 
 function Skills() {
 
@@ -15,9 +27,31 @@ return (
             <ContainerSkills>
                 <ContainerTechnicalSkills>
                     <TitleTechnicalSkills>Habilidades técnicas</TitleTechnicalSkills>
+                    <SubContainerTechnicalSkills>
+
+                    </SubContainerTechnicalSkills>
                 </ContainerTechnicalSkills>
                 <ContainerSoftSkills>
                     <TitleSoftSkills>Habilidades blandas</TitleSoftSkills>
+                    <SubContainerSoftSkills>
+                        <ContainerIllustrationTitle>
+                            <Illustration src='./public/skills/adaptacion.png' alt='adaptacion'/>
+                            <TitleIllustration>ADAPTACIÓN</TitleIllustration>
+                        </ContainerIllustrationTitle>
+                        <ContainerIllustrationTitle>
+                            <Illustration src='./public/skills/resolucion-de-problemas.png' alt='resolucion-de-problemas'/>
+                            <TitleIllustration>RESOLUCIÓN DE PROBLEMAS</TitleIllustration>
+                        </ContainerIllustrationTitle>
+                        <ContainerIllustrationTitle>
+                            <Illustration src='./public/skills/trabajo-en-equipo.png' alt='trabajo-en-equipo'/>
+                            <TitleIllustration>TRABAJO EN EQUIPO</TitleIllustration>
+                        </ContainerIllustrationTitle>
+                        <ContainerIllustrationTitle>
+                            <Illustration src='./public/skills/creatividad.png' alt='creatividad'/>
+                            <TitleIllustration>CREATIVIDAD</TitleIllustration>
+                        </ContainerIllustrationTitle>
+
+                    </SubContainerSoftSkills>
                 </ContainerSoftSkills>
             </ContainerSkills>
         </Container>
--- src/components/skills/skillsstyled.js
+++ src/components/skills/skillsstyled.js
@@ -12,7 +12,7 @@ export const Container = styled.section`
     }
 `
 export const ContainerSkills = styled.div`
-    width: 70%;
+    width: 80%;
     display: flex;
     gap: 3rem;
 `
@@ -27,11 +27,20 @@ export const TitleTechnicalSkills = styled.h3`
     background: linear-gradient(90deg, rgba(190, 149, 196, 1) 0%, rgba(159, 134, 192, 1) 41%, rgba(224, 177, 203, 1) 100%);
     background-clip: text;
     color: transparent;
+    text-align: center;
+`
+export const SubContainerTechnicalSkills = styled.div`
+    width: 90%;
+    display: flex;
+    justify-content: center;
+    flex-wrap: nowrap;
 `
 export const ContainerSoftSkills = styled.div`
     width: 50%;
     display: flex;
     flex-direction: column;
+    align-items: center;
+    gap: 2.5rem;
 `
 export const TitleSoftSkills = styled.h3`
     font-size: 35px;
@@ -39,4 +48,34 @@ export const TitleSoftSkills = styled.h3`
     background: linear-gradient(90deg, rgba(190, 149, 196, 1) 0%, rgba(159, 134, 192, 1) 41%, rgba(224, 177, 203, 1) 100%);
     background-clip: text;
     color: transparent;
+    padding: 1rem;
+    text-align: center;
+`
+export const SubContainerSoftSkills = styled.div`
+    width: 90%;
+    height: auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    overflow-y: auto;
+`
+export const ContainerIllustrationTitle = styled.div`
+    width: 150px;
+    height: 180px;
+    padding: 1rem;
+
+`
+export const Illustration = styled.img`
+    width: 120px;
+    height: 110px;
+    padding: 1rem;
+    background-color:rgb(59, 58, 58);
+`
+export const TitleIllustration = styled.h4`
+    font-size: 11px;
+    font-weight: lighter;
+    margin: 0;
+    color: #959595;
+    text-align: center;
+
 `
