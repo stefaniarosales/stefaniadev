c30f8c06ca3aeac71adfb7285a38fa1f2e87f727
stefaniarosales <stefaniarosales@live.com>
2025-01-14 21:16:07 -0300

    mod: vh start

--- src/components/start/startstyled.js
+++ src/components/start/startstyled.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.section`
     width: 100%;
-    height: 100vh;
     display: flex;
     flex-direction: column;
     justify-content: center;
