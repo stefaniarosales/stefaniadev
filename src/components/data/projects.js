00d36924ef0f6ce065535e6487de981aa5891fce
stefaniarosales <stefaniarosales@live.com>
2025-01-27 14:46:52 -0300

    add: img projects png

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -5,8 +5,8 @@ export const projects = [
       git: "https://github.com/stefaniarosales/pottermania.git",
       description: "He desarrollado una aplicación web utilizando React como pilar fundamental. Con React Router DOM, he logrado una navegación fluida entre las diferentes secciones. Para gestionar el estado de la aplicación, he empleado Redux y Redux Toolkit, optimizando el manejo del carrito, menú y otros elementos. Formik y Yup me han asistido en la creación de formularios robustos y bien validados. Styled-components ha sido esencial para darle estilo a la interfaz de usuario, mientras que SweetAlert ha enriquecido la experiencia del usuario con mensajes personalizados. Redux Persist garantiza la persistencia de los datos del usuario, incluso al recargar la página.",
       technologies: ["HTML","CSS","React","styled-components","Git",],
-      image: '/projects/harrypotter.webp',
-      mobileImage: "/projects/harrypotter-mobile.webp"
+      image: '/projects/pottermania-web.png',
+      mobileImage: "/projects/pottermania-mobile.png"
     },
     {
       title: "Marvelmania",
@@ -14,8 +14,8 @@ export const projects = [
       git: "https://github.com/stefaniarosales/MarvelMania.git",
       description: "He diseñado una landing page intuitiva y fácil de usar, construida con HTML, CSS y JavaScript. El carrito de compras es una de las características principales, permitiendo a los usuarios personalizar sus pedidos con facilidad. Con solo unos clics, pueden agregar productos, modificar cantidades y eliminar elementos. Además, he incluido un menú hamburguesa que se adapta a cualquier tamaño de pantalla, y un formulario de contacto que guía al usuario a través del proceso de envío de mensajes.",
       technologies: ["HTML","CSS","Javascript","Git"],
-      image: '/projects/marvelmania.webp',
-      mobileImage: "/projects/marvelmania-mobile.webp"
+      image: '/projects/marvelmania-web.png',
+      mobileImage: "/projects/marvelmania-mobile.png"
     },
     {
       title: "Kyra",
@@ -23,7 +23,7 @@ export const projects = [
       git: "https://github.com/stefaniarosales/Integrador-DisenoWeb.git",
       description: "El objetivo era crear una página web limpia y estéticamente agradable, utilizando una paleta de colores reducida y tipografías sencillas. Durante el desarrollo, exploré diferentes técnicas de diseño responsivo, como media queries y flexbox. Este proyecto me permitió afianzar mis conocimientos en diseño de interfaces de usuario y mejorar mi capacidad para crear layouts flexibles y adaptables.",
       technologies: ["HTML","CSS","Git"],
-      image: '/projects/kyra.webp',
-      mobileImage: "/projects/kyra-mobile.webp"
+      image: '/projects/kyra-web.png',
+      mobileImage: "/projects/kyra-mobile.png"
     },
   ];
